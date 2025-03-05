cc23e46d5d5fa8891af80452cbfa39faf3e90366
Lima Motos <contato.limamotos@gmail.com>
2025-03-05 16:48:54 -0300

    agora ce vaiiii

--- limamotos/settings.py
+++ limamotos/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-=======
+
 """
 Django settings for limamotos project.
 
